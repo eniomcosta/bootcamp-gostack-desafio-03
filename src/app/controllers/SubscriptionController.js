e7a7f618c3059929d7d977f9145eaccf20aa1ad1
Enio Costa <enio.m.costa@gmail.com>
2019-09-06 21:49:21 -0300

    Send mail to the organizer after subscription on a meetup

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -1,5 +1,9 @@
 import SubscriptionService from '../services/SubscriptionService';
 import Subscription from '../models/Subscription';
+import Meetup from '../models/Meetup';
+import User from '../models/User';
+
+import Mail from '../../lib/Mail';
 
 class SubscriptionController {
   async store(req, res) {
@@ -12,6 +16,22 @@ class SubscriptionController {
     const subscription = await Subscription.create({
       meetup_id: req.body.meetup,
       user_id: req.userId,
+    });
+
+    const selectedMeetup = await Meetup.findByPk(req.body.meetup, {
+      include: [
+        {
+          model: User,
+          as: 'organizer',
+          attributes: ['id', 'name', 'email'],
+        },
+      ],
+    });
+
+    await Mail.sendMail({
+      to: `${selectedMeetup.organizer.name} <${selectedMeetup.organizer.email}>`,
+      subject: `Subscription to ${selectedMeetup.title}`,
+      text: 'A subscription has been made to a the meetup',
     });
 
     return res.json(subscription);
---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '91685f59419011',
+    pass: '76afe484e60ceb',
+  },
+  default: {
+    from: 'Equipe Meetup App <noreply@meetupapp.com>',
+  },
+};
